94a18fc0865512d513bc2ca58234669ccaadc5ee
nikos <59179110+nikosstam4@users.noreply.github.com>
2022-06-17 17:29:36 +0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -4,7 +4,6 @@ VALUES ('The_times','daily','Spuropoulos Andreas'),
 ('The_news','daily','Papageorgiou Maria'),
 ('Your_voice','weekly','Stamopoulos Nikolaos');
 
-
 INSERT INTO paper(Name, Paper_number, Release_date, Pages)
 VALUES ('The_times',3566,'2019-10-9',34),
 ('The_news',786,'2019-9-9',30),
@@ -12,13 +11,11 @@ VALUES ('The_times',3566,'2019-10-9',34),
 ('The_news',787,'2019-9-10',32),
 ('Your_voice',108,'2019-12-18',56);
 
-
 INSERT INTO category(Id, Name, Description, Parental) 
 VALUES(15,'Foreign Policy','The background of politics','Politics'),
 (14,'Internal Policy','The background of politics','Politics'),
 (17,'Sports','All the sports',default),
 (10,'Social','The Problem with Social Classes',default);
-
 
 INSERT INTO article(Path, Title, Summary, Article_id, Page_name, Page_num, Cover, Extent)
 VALUES ('C:\Users\Articles\Submitted\Article34a.doc','Exceeded loans','New changes for exceeded loans',14,'The_times',3566,5,6),
@@ -26,13 +23,11 @@ VALUES ('C:\Users\Articles\Submitted\Article34a.doc','Exceeded loans','New chang
 ('C:\Users\Articles\Submitted\Article798a.doc','Big talk','Erdogan speech',15,'The_news',786,9,3),
 ('C:\Users\Articles\Submitted\Article245b.doc','Conserts of 2020','Information about the upcoming artists',10,'Your_voice',107,18,3);
 
-
 INSERT INTO key(Article, Word_id)
 VALUES('C:\Users\Articles\Submitted\Article34a.doc','loans'),
 ('C:\Users\Articles\Submitted\Article78a.doc','Olympic_games'),
 ('C:\Users\Articles\Submitted\Article798a.doc','Turkey'),
 ('C:\Users\Articles\Submitted\Article245b.doc','conserts');
-
 
 INSERT INTO employee(email, Name, Last_name, Salary, Recruitment_date, News_name)
 VALUES('giwrgos43@gmail.com','Georgios','Georgiou',1300,'2018-8-9','The_times'),
@@ -43,11 +38,9 @@ VALUES('giwrgos43@gmail.com','Georgios','Georgiou',1300,'2018-8-9','The_times'),
 ('voulatoudim@gmail.com','Dimitra','Voulatou',2300,'2017-6-12','The_times'),
 ('kwstantinosv@outlook.com','Vasilis','Constantinou',2300,'2017-6-9','Your_voice');
 
-
 INSERT INTO administrator(Email, duties , Street, City, Number)
 VALUES('voulatoudim@gmail.com','Secretary','Korinthou','Patras',45),
 ('kwstantinosv@outlook.com','Logistics','Ermou','Patras',59);
-
 
 INSERT INTO adm_phone_num(Administrative, Phone_num)
 VALUES('kwstantinosv@outlook.com',6988996206),
@@ -55,12 +48,10 @@ VALUES('kwstantinosv@outlook.com',6988996206),
 ('kwstantinosv@outlook.com',6972326658),
 ('voulatoudim@gmail.com',6987983367);
 
-
 INSERT INTO journalist(Email, CV, Work_experience)
 VALUES('giwrgos43@gmail.com','Objective, Strong character, Journalism in IEK AKMH',6),
 ('nikosp@hotmail.com','Impartial, Social, manchester metropolitan university diploma',12),
 ('gwgwspanou2@gmail.com','Powerful , Journalism and Law diplomas',18);
-
 
 INSERT INTO submited_by(Email, Path, Date)
 VALUES('giwrgos43@gmail.com','C:\Users\Articles\Submitted\Article34a.doc','2019-10-6'),
@@ -68,13 +59,11 @@ VALUES('giwrgos43@gmail.com','C:\Users\Articles\Submitted\Article34a.doc','2019-
 ('nikosp@hotmail.com','C:\Users\Articles\Submitted\Article245b.doc','2019-12-1'),
 ('gwgwspanou2@gmail.com','C:\Users\Articles\Submitted\Article798a.doc','2019-11-30');
 
-
 INSERT INTO editor(Path, Email, Name, Paper_number, Page)
 VALUES('C:\Users\Articles\Submitted\Article798a.doc','kwnnaspurou@outlook.com','The_news',786,2),
 ('C:\Users\Articles\Submitted\Article78a.doc','kwnnaspurou@outlook.com','The_times',3566,9),
 ('C:\Users\Articles\Submitted\Article34a.doc','kwnnaspurou@outlook.com','The_times',3566,10),
 ('C:\Users\Articles\Submitted\Article245b.doc','lemonis@hotmail.com','Your_voice',107,28);
-
 
 INSERT INTO compose(Email, Path, Approval, Resubmit, Changes,Approval_date)
 VALUES('kwnnaspurou@outlook.com','C:\Users\Articles\Submitted\Article798a.doc',1,0,NULL,2019-07-28),
